fe4c32355b79cd33b78b63248834b9f9881e3f9e
Yutaro SHIRAI <60741947+tarowhitey@users.noreply.github.com>
2025-12-25 00:56:13 +0900

    feat: setup tailwind css and github actions workflows

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./*.php",
+    "./templates/**/*.php",
+    "./parts/**/*.php",
+    "./inc/**/*.php",
+    "./src/js/**/*.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
